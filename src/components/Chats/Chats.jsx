86536261053936aeae57e6827d4d5e45d6bd13d3
Sanchi Bansal <sanchibansal340@gmail.com>
2021-08-15 22:44:49 +0530

    Fix landing page UI

--- src/components/Chats/Chats.jsx
+++ src/components/Chats/Chats.jsx
@@ -91,6 +91,7 @@ function Chats() {
                 projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
                 userName={user.email}
                 userSecret={user.uid}
+                offset={6}
             />
         </div>
     )
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,22 +4,32 @@ import firebase from 'firebase';
 import { GoogleOutlined } from '@ant-design/icons';
 
 import './Login.scss';
+import LandingImg from '../../assets/images/landing.jpg';
 
 function Login() {
     return (
-        <div className="Login container-fluid">
-            <div className="row justify-content-center align-items-center">
-                <div className="col-md-6 card align-items-center py-3">
-                    <h3 className="text-center mb-3">
+        <div className="Login container-fluid pt-5">
+            <div className="row">
+                <div className="col-md-6 my-auto text-center">
+                    <h3 className="mb-1">
                         Welcome to Chit-Chat!
                     </h3>
+
+                    <h5 className="text-muted">
+                        For those who love to talk, but just behind the screens :)
+                        <br /><br />
+                        Now connect with your loved ones with just one click! 
+                    </h5>
                     
                     <div 
-                        className="btn btn-primary py-2"
+                        className="btn btn-warning py-2 mt-4"
                         onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
                     >
                         <GoogleOutlined /> Sign In with Google
                     </div>
+                </div>
+                <div className="col-md-6 text-center py-3 ">
+                    <img src={LandingImg} alt="Landing Chatting" className="img-fluid" />
                 </div>
             </div>
         </div>
